d6e1f9b166605fe336012d93d391f347ab513786
unknown <i.penju1022@gmail.com>
2021-11-09 14:09:00 +0900

    chap3-12

---
+++ Desktop/python2年生/chap3/chap3-12.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+#csvファイルを読み込む
+df = pd.read_csv("test.csv")
+
+#ソート(国語の点数が高いもの順)
+kokugo = df.sort_values("国語",ascending=False)
+
+#csvファイルに出力する
+kokugo.to_csv("export2.csv", index=False)
